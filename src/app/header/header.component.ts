f5a4afba65a9a5a691b4d5e744442bb654e68380
Pham <Baccarani.Pham@capco.com>
2022-09-03 11:38:03 -0400

    Refactored home page to be web-mobile responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMenuOpen = false;
+  mobile = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngOnInit() {
+    if (window.screen.width < 400) { // 768px portrait
+      this.mobile = true;
+    }
+  }
 
 
 }
